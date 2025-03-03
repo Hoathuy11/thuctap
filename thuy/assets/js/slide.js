b02b523b19c595a9dce7b59c0557ca89dac8485e
Hoathuy <nguyenhoathuy141103@gmail.com>
2025-03-03 09:26:31 +0700

    update

--- thuy/assets/js/slide.js
+++ thuy/assets/js/slide.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     $('.slider').slick({
-        infinite:true,
-        slidesToShow:4,
-        slidesToScroll:1,
-        prevArrow:".sec-slide .prev",
-        nextArrow:".sec-slide .next"
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: ".sec-slide .prev",
+        nextArrow: ".sec-slide .next",
+        responsive: [
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 });
